56481f715c2cb1ab81dde6f20d59755a580b0423
Gaurav Pilania <gpilania9@gmail.com>
2025-06-25 10:07:10 +0530

    Add files via upload

---
+++ task_2_assignment_1_python_module.py
@@ -0,0 +1,10 @@
+#ASSIGNMENT 1 (TASK-2):Create a Personalized Greeting
+
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+
+#concatination
+full_name=first_name+" "+last_name
+
+#display the personalised message:
+print("Hello, " + full_name +"! Welcome to the Python program.")
