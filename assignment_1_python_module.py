5c6533be9b925be859334abd01b86eae9b0d2455
gpilania18 <gpilania9@gmail.com>
2025-06-25 09:44:04 +0530

    Add files via upload

---
+++ assignment_1_python_module.py
@@ -0,0 +1,32 @@
+# ASSIGNMENT 1 : Perform Basic Mathematical Operations
+
+first_no=float(input("Enter the first number:"))
+second_no=float(input("Enter the second number:"))
+
+#operations on the numbers
+add=first_no+second_no
+sub=first_no-second_no
+multiply=first_no*second_no
+divide=first_no/second_no
+
+
+#displaying the outputs:
+
+print("Addition: ",add)
+print("Substraction: ",sub)
+print("Multiplication: ",multiply)
+print("Division: ",divide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
